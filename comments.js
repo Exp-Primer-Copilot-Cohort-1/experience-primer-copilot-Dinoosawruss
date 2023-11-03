fb86b7d60cf69e325b5414070de0ee67cc802bc9
Michael D. <m.dormand@sky.com>
2023-11-03 15:01:53 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,13 @@
+// Create web server
+const http = require('http');
+
+const port = 3000; // You can choose any available port
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, World!\n');
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
